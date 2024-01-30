93445c33fef88ee63ea5df0b1547b0a2510aeccb
MarioFerreira <mario@entregali.com.br>
2024-01-30 18:44:51 -0300

    fix/fix and improve styles

--- src/components/Flex.tsx
+++ src/components/Flex.tsx
@@ -2,29 +2,12 @@ import React from "react";
 import clsx from "clsx";
 
 interface FlexProps {
-  display?: "flex" | "grid" | "none";
-  justifyContent?: "start" | "end" | "center" | "between" | "around";
-  alignItems?: "start" | "end" | "center" | "baseline" | "stretch";
-  direction?: "row" | "row-reverse" | "col" | "col-reverse";
   className?: string;
   children?: React.ReactNode;
 }
 
-const Flex: React.FC<FlexProps> = ({
-  display,
-  justifyContent,
-  alignItems,
-  direction,
-  className,
-  children,
-}) => {
-  const flexClasses = clsx(
-    display,
-    className,
-    justifyContent && `justify-${justifyContent}`,
-    alignItems && `items-${alignItems}`,
-    direction && `flex-${direction}`
-  );
+const Flex: React.FC<FlexProps> = ({ className, children }) => {
+  const flexClasses = clsx(className);
 
   return <div className={flexClasses}>{children}</div>;
 };
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -3,13 +3,7 @@ import { Button } from "react-daisyui";
 
 export default function Home() {
   return (
-    <Flex
-      display="flex"
-      justifyContent="center"
-      alignItems="center"
-      direction="col"
-      className="h-screen gap-4"
-    >
+    <Flex className="flex flex-col items-center justify-center h-screen gap-4">
       <Button>daisyui import</Button>
       <button className="btn">daisyUI classname</button>
     </Flex>
